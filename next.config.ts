5a4f378c25756a2cc4874cd13d403c29b82f5eaa
Jose <75870284+Jaro-c@users.noreply.github.com>
2025-03-11 16:03:08 -0700

    chore: disable development indicators in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	compress: false, // Nginx responsable de la comprensión
+	devIndicators: false, // No mostrar indicadores de desarrollo
 	poweredByHeader: false,
 	experimental: {
 		optimizePackageImports: [],
