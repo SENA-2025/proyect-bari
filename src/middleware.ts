fdc5324bab58b3ad392816de55f5657c4e3b69a4
Jose <75870284+Jaro-c@users.noreply.github.com>
2025-03-10 21:04:02 -0700

    feat: add middleware for enhanced security and request handling

---
+++ src/middleware.ts
@@ -0,0 +1,66 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+	// Obtener la URL de la solicitud
+	const getUrl = request.nextUrl.pathname.toLowerCase();
+
+	// Generar un nonce aleatorio
+	const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
+	const CSP = `
+		default-src 'self';
+		connect-src 'self';
+		script-src 'self' 'nonce-${nonce}' ${process.env.NODE_ENV === "development" ? "'unsafe-eval'" : "'strict-dynamic'"};
+		script-src-attr 'self' 'nonce-${nonce}';
+		script-src-elem 'self' 'nonce-${nonce}';
+		img-src 'self' blob: data:;
+		font-src 'self';
+		object-src 'none';
+		base-uri 'self';
+		form-action 'self';
+		frame-ancestors 'none';
+		worker-src 'self';
+		style-src 'self' 'nonce-${nonce}';
+		style-src-attr 'self' 'nonce-${nonce}';
+		style-src-elem 'self' 'nonce-${nonce}';
+		upgrade-insecure-requests;`;
+	const CSP_Header = CSP.replace(/\s{2,}/g, " ").trim();
+
+	// Encabezados de la solicitud
+	const requestHeaders = new Headers(request.headers);
+	requestHeaders.set("x-nonce", nonce);
+	requestHeaders.set("Content-Security-Policy", CSP_Header);
+
+	// Validacion de Autenticacion
+	// TODO: Implementar validacion de autenticacion
+
+	// Respuesta de Next.js
+	const response = NextResponse.next({ request: { headers: requestHeaders } });
+
+	response.headers.set("Content-Security-Policy", CSP_Header);
+	response.headers.set("X-XSS-Protection", "1; mode=block");
+	response.headers.set("X-Permitted-Cross-Domain-Policies", "none");
+	response.headers.set("X-Frame-Options", "DENY");
+	response.headers.set("X-Download-Options", "noopen");
+	response.headers.set("X-Content-Type-Options", "nosniff");
+
+	response.headers.set("Referrer-Policy", "same-origin");
+	response.headers.set("Origin-Agent-Cluster", "?1");
+	response.headers.set("Cross-Origin-Resource-Policy", "same-origin");
+	response.headers.set("Cross-Origin-Opener-Policy", "same-origin");
+	response.headers.set("Cross-Origin-Embedder-Policy", "require-corp");
+
+	return response;
+}
+
+export const config = {
+	matcher: [
+		{
+			source: "/((?!_next/static|_next/image|favicon.ico|robots.txt).*)",
+			missing: [
+				{ type: "header", key: "next-router-prefetch" },
+				{ type: "header", key: "purpose", value: "prefetch" },
+			],
+		},
+	],
+};
