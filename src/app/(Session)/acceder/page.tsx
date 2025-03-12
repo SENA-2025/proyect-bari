2d63f7f2691312da5b778344671354bfb759aea8
Jose <75870284+Jaro-c@users.noreply.github.com>
2025-03-11 17:38:47 -0700

    feat: add login page component and update favicon

---
+++ src/app/(Session)/acceder/page.tsx
@@ -0,0 +1,45 @@
+import type { Metadata } from "next";
+import dynamic from "next/dynamic";
+import Image from "next/image";
+import Link from "next/link";
+
+// Metadata
+// TODO: Mejorar el metadata
+export const metadata: Metadata = {
+	title: "Acceder",
+	alternates: { canonical: "acceder" },
+};
+
+// Componentes
+const Form = dynamic(() => import("@/components/(Session)/acceder/form"));
+
+// Página
+export default function Login_Page() {
+	return (
+		<div className="bg-white size-full">
+			{/* Formulario */}
+			<div className="flex items-center justify-center h-full">
+				<div className="backdrop-blur-sm shadow-lg p-8 rounded-xl bg-white/90">
+					<div className="flex flex-col items-center gap-4">
+						{/* Logo & Título */}
+						<div className="flex flex-col items-center gap-8">
+							<Image className="shadow-xs rounded-full select-none" src={"/logo.webp"} alt="Logo del SENA" width={100} height={100} priority />
+							<h1 className="font-semibold text-2xl lg:text-3xl select-none">Iniciar Sesión</h1>
+						</div>
+
+						{/* Formulario */}
+						<Form />
+
+						{/* Registro */}
+						<p className="text-xs lg:text-sm text-secondary-600 select-none">
+							¿No tienes una cuenta?{" "}
+							<Link href="/registrarse" className="text-tertiary-600 hover:text-primary-400 font-medium ease-in-out duration-300 transition-colors">
+								Regístrate aquí
+							</Link>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
