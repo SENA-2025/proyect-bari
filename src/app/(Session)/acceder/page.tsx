7d03f8f7c6a998b10b9beb2df48adf670d78a8fa
Jose <75870284+Jaro-c@users.noreply.github.com>
2025-03-11 20:31:48 -0700

    style: update text color for login page title for better visibility

--- src/app/(Session)/acceder/page.tsx
+++ src/app/(Session)/acceder/page.tsx
@@ -25,7 +25,7 @@ export default function Login_Page() {
 							{/* Logo & Título */}
 							<div className="flex flex-col items-center gap-8">
 								<Image className="shadow-xs rounded-full select-none" src={"/logo.webp"} alt="Logo del SENA" width={100} height={100} priority />
-								<h1 className="font-semibold text-2xl lg:text-3xl select-none">Iniciar Sesión</h1>
+								<h1 className="font-semibold text-gray-800 text-2xl lg:text-3xl select-none">Iniciar Sesión</h1>
 							</div>
 
 							{/* Formulario */}
