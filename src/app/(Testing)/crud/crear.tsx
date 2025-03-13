5219bce0345f775938d956cb4d3188653a9ca7b7
Jose <75870284+Jaro-c@users.noreply.github.com>
2025-03-13 16:36:09 -0700

    feat: remove unused CRUD components and related files

--- src/app/(Testing)/crud/crear.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function Crear_Componente() {
-	return (
-		<div>
-			<button className="bg-primary-400 text-white py-2 px-4 rounded-lg">Nuevo</button>
-		</div>
-	);
-}
