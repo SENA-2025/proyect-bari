b59edf7a363e6a6fa703e015ce9fe523abfca718
Jose <75870284+Jaro-c@users.noreply.github.com>
2025-03-12 21:29:04 -0700

    feat: add CRUD components for data creation and display

---
+++ src/app/(Testing)/crud/crear.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Crear_Componente() {
+	return (
+		<div>
+			<button className="bg-primary-400 text-white py-2 px-4 rounded-lg">Nuevo</button>
+		</div>
+	);
+}
