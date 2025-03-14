f3756c25b4d21827c111eefb5d782f9518763ca6
Jose <75870284+Jaro-c@users.noreply.github.com>
2025-03-13 22:29:42 -0700

    refactor: remove unused fields from ServiceType and clean up ServiceLogin function

--- src/services/(Session)/acceder/action.ts
+++ src/services/(Session)/acceder/action.ts
@@ -1,15 +1,12 @@
 "use server";
 
+// Tipos
 export type ServiceType = {
 	error: boolean;
 	message: string | null;
-	fields?: {
-		type: string;
-		number: string;
-		password: string;
-	} | null;
 };
 
+// Validación de Datos
 export async function ServiceLogin(formData: FormData): Promise<ServiceType> {
 	// Validar: Datos Obligatorios
 	const keysRequired = ["document_type", "document_number", "password"];
@@ -17,12 +14,11 @@ export async function ServiceLogin(formData: FormData): Promise<ServiceType> {
 		return { error: true, message: "Complete los campos" };
 	}
 
-
-	console.log(formData)
+	console.log(formData);
 
 	// Enviar Datos
 	return {
 		error: true,
 		message: "¡Bienvenido!",
 	};
-};
+}
