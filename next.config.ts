6cb57010b97ae7ca84bb6ef02d55a52378a59614
Jose <75870284+Jaro-c@users.noreply.github.com>
2025-03-18 13:47:01 -0700

    feat: disable ETag generation for improved caching management

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	compress: false, // Nginx responsable de la comprensión
 	devIndicators: false, // No mostrar indicadores de desarrollo
-	poweredByHeader: false,
+	poweredByHeader: false, // No mostrar el encabezado de Next.js
+	generateEtags: false, // No generar ETags (Cache gestionada por Cloudflare)
 	experimental: {
 		optimizePackageImports: ["uuid", "zod", "react-hot-toast"],
 	},
