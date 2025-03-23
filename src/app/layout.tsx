a01f2955ec138fa8a11047385339bd46020e747a
Jose <75870284+Jaro-c@users.noreply.github.com>
2025-03-23 13:35:40 -0700

    feat: adjust toast notification durations for success and error messages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,12 +56,14 @@ export default function Root_Layout({ children }: Readonly<{ children: React.Rea
 					toastOptions={{
 						duration: 5000,
 						success: {
+							duration: 2000,
 							icon: <CircleCheck className="size-5 shrink-0 text-green-500" />,
 							className:
 								"z-50 flex items-center gap-2 bg-white text-gray-800 border border-green-300 shadow-md rounded-lg px-4 py-3 text-xs lg:text-sm xl:text-base font-medium select-none mx-2 lg:mx-4 mb-4",
 						},
 						loading: {},
 						error: {
+							duration: 4000,
 							icon: <CircleX className="size-5 shrink-0 text-red-500" />,
 							className:
 								"z-50 flex items-center gap-2 bg-white text-gray-800 border border-red-300 shadow-md rounded-lg px-4 py-3 text-xs lg:text-sm xl:text-base font-medium select-none mx-2 lg:mx-4 mb-4",
