a6427012af04ef581c1f88483af0b0963dd58328
Jose <75870284+Jaro-c@users.noreply.github.com>
2025-03-24 15:53:19 -0700

    fix: enhance toast notification for login form and add redirection to password reset

--- src/components/(Sesion)/acceder/form.tsx
+++ src/components/(Sesion)/acceder/form.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { ChevronDown, Eye, EyeOff } from "lucide-react";
+import { ChevronDown, Eye, EyeOff, TriangleAlert } from "lucide-react";
 import Form from "next/form";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { useActionState, useEffect, useState } from "react";
 import { toast } from "react-hot-toast";
 
@@ -18,6 +19,7 @@ function adapter(_state: ServiceType, formData: FormData): Promise<ServiceType> 
 
 // Componente
 export default function Login_Form() {
+	const router = useRouter();
 	const [userData, setUserData] = useState({
 		document_type: "CC",
 		document_number: "",
@@ -62,7 +64,22 @@ export default function Login_Form() {
 			});
 
 			// Mostrar Mensaje
-			toast.success(state.message);
+			if (state.message === "OK") {
+			} else {
+				toast.success(state.message, {
+					duration: 2000,
+					icon: <TriangleAlert className="size-5 shrink-0 text-orange-500" />,
+					className:
+						"z-50 flex items-center gap-2 bg-white text-gray-800 border border-orange-300 shadow-md rounded-lg px-3 lg:px-4 py-2 lg:py-3 text-xs lg:text-sm xl:text-base font-medium select-none mx-2 lg:mx-4 mb-4",
+				});
+
+				// Redirigir después de 3 segundos
+				const timeout = setTimeout(() => {
+					router.push("/restablecer-clave");
+				}, 3000);
+
+				return () => clearTimeout(timeout);
+			}
 		}
 
 		return undefined;
