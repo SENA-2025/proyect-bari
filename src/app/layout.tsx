256b487d23aec03a4f3761cba342d41a1fa39206
Jose <75870284+Jaro-c@users.noreply.github.com>
2025-03-24 08:45:15 -0700

    fix: dynamically import Toaster component for improved performance

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import "./globals.css";
 
-import { CircleX, CircleCheck } from "lucide-react";
 import type { Metadata, Viewport } from "next";
-import { Toaster } from "react-hot-toast";
+import dynamic from "next/dynamic";
 
 // Metadata
 export const metadata: Metadata = {
@@ -40,6 +39,9 @@ const poppins = Poppins({
 	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
+// Componentes
+const Toaster = dynamic(() => import("@/components/toaster"));
+
 // Layout
 export default function Root_Layout({ children }: Readonly<{ children: React.ReactNode }>) {
 	return (
@@ -49,27 +51,7 @@ export default function Root_Layout({ children }: Readonly<{ children: React.Rea
 					<div className="w-full grow">{children}</div>
 				</main>
 
-				{/* Toaster */}
-				<Toaster
-					position="bottom-right"
-					gutter={-8}
-					toastOptions={{
-						duration: 5000,
-						success: {
-							duration: 2000,
-							icon: <CircleCheck className="size-5 shrink-0 text-green-500" />,
-							className:
-								"z-50 flex items-center gap-2 bg-white text-gray-800 border border-green-300 shadow-md rounded-lg px-3 lg:px-4 py-2 lg:py-3 text-xs lg:text-sm xl:text-base font-medium select-none mx-2 lg:mx-4 mb-4",
-						},
-						loading: {},
-						error: {
-							duration: 4000,
-							icon: <CircleX className="size-5 shrink-0 text-red-500" />,
-							className:
-								"z-50 flex items-center gap-2 bg-white text-gray-800 border border-red-300 shadow-md rounded-lg px-3 lg:px-4 py-2 lg:py-3 text-xs lg:text-sm xl:text-base font-medium select-none mx-2 lg:mx-4 mb-4",
-						},
-					}}
-				/>
+				<Toaster />
 			</body>
 		</html>
 	);
