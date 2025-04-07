9469e83e22cdab71413e9cd57db56220bdf7a24e
Jose <75870284+Jaro-c@users.noreply.github.com>
2025-04-06 19:42:22 -0700

    feat(layout): restructure layout to include Sidebar and Footer components for improved organization

--- src/app/(Privado)/app/layout.tsx
+++ src/app/(Privado)/app/layout.tsx
@@ -3,6 +3,8 @@ import { cookies } from "next/headers";
 
 // Componentes
 const Header = dynamic(() => import("@/components/UI/Navegacion/header"));
+const Sidebar = dynamic(() => import("@/components/UI/Navegacion/sidebar"));
+const Footer = dynamic(() => import("@/components/UI/Navegacion/footer"));
 const AccessRefresher = dynamic(() => import("@/components/(Privado)/AccessRefresher"));
 
 // Layout
@@ -15,13 +17,25 @@ export default async function AppLayout({ children }: Readonly<{ children: React
 			{!cookieStore.has("_sid") && <AccessRefresher />}
 
 			{/* Layout */}
-			<div className="flex size-full flex-col bg-white/95 backdrop-blur-sm">
+			<div className="flex size-full flex-col bg-white/95">
 				{/* Header */}
 				<Header />
 
 				{/* Contenido */}
 				<div className="h-full grow overflow-hidden">
-					<div>{children}</div>
+					<div className="flex size-full">
+						{/* Sidebar */}
+						<Sidebar />
+
+						{/* Contenido Principal */}
+						<div className="flex size-full flex-col justify-between">
+							{/* Contenido */}
+							<div className="size-full">{children}</div>
+
+							{/* Footer */}
+							<Footer />
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
