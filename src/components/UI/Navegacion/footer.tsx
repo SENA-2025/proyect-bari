8bef0ef0c58b8a0cede30b2602006bcff0ca0094
Jose <75870284+Jaro-c@users.noreply.github.com>
2025-04-07 16:14:38 -0700

    🎨 style(footer): adjust padding in version display and wrap CurrentDate in Suspense for better loading handling

--- src/components/UI/Navegacion/footer.tsx
+++ src/components/UI/Navegacion/footer.tsx
@@ -1,5 +1,6 @@
 import { Tag } from "lucide-react";
 import dynamic from "next/dynamic";
+import { Suspense } from "react";
 
 // Componentes
 const CurrentDate = dynamic(() => import("@/components/UI/Navegacion/footer/date"));
@@ -12,13 +13,15 @@ export default function Footer() {
 			<div className="mx-4">
 				<div className="flex items-center justify-between select-none">
 					{/* Version */}
-					<div className="flex items-center gap-1 rounded bg-gray-100 px-2 py-1">
+					<div className="flex items-center gap-1 rounded bg-gray-100 px-3 py-1">
 						<Tag className="text-tertiary-600" size={14} />
 						<span className="text-tertiary-600 text-xs font-medium lg:text-sm">v{process.env.APP_VERSION}</span>
 					</div>
 
 					{/* Tiempo */}
-					<CurrentDate initialDate={initialDate} />
+					<Suspense>
+						<CurrentDate initialDate={initialDate} />
+					</Suspense>
 				</div>
 			</div>
 		</footer>
