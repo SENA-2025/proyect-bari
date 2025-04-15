70159060fc2dc77c1ad08259097a3c9800111548
Jose <75870284+Jaro-c@users.noreply.github.com>
2025-04-14 18:20:15 -0700

    ✨ feat(sidebar): wrap Desktop component in Suspense for improved loading handling

--- src/components/UI/Navegacion/sidebar.tsx
+++ src/components/UI/Navegacion/sidebar.tsx
@@ -1,5 +1,25 @@
 import dynamic from "next/dynamic";
+import { Suspense } from "react";
+
+// Componentes
+const Desktop = dynamic(() => import("@/components/UI/Navegacion/sidebar/desktop"));
 
 export default function Sidebar() {
-	return <aside className="hidden lg:block">T</aside>;
+	return (
+		<aside className="hidden h-full lg:block">
+			<Suspense
+				fallback={
+					<div className="z-10 flex h-full w-14 flex-col gap-2 bg-white px-2 py-4 shadow-lg">
+						{Array(3)
+							.fill(0)
+							.map((_, index) => (
+								<div className="h-10 w-full animate-pulse rounded-lg bg-gray-100" key={index}></div>
+							))}
+					</div>
+				}
+			>
+				<Desktop />
+			</Suspense>
+		</aside>
+	);
 }
