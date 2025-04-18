17da554b99aa45d27366aa916ccda4040400267f
Jose <75870284+Jaro-c@users.noreply.github.com>
2025-04-18 11:45:52 -0700

    💄 style(layout): remove background color from loading indicator in AppLayout component

--- src/app/(Privado)/app/layout.tsx
+++ src/app/(Privado)/app/layout.tsx
@@ -37,7 +37,7 @@ export default async function AppLayout({ children }: Readonly<{ children: React
 									{cookieStore.has("_sid") ? (
 										children
 									) : (
-										<div className="flex size-full items-center justify-center bg-gray-50">
+										<div className="flex size-full items-center justify-center">
 											<div className="flex w-full flex-col items-center justify-center gap-4">
 												<div className="border-t-primary-400 text-primary-400 flex h-20 w-20 animate-spin items-center justify-center rounded-full border-4 border-transparent text-4xl">
 													<div className="border-t-secondary-600 text-secondary-600 flex h-16 w-16 animate-spin items-center justify-center rounded-full border-4 border-transparent text-2xl"></div>
