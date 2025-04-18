36b14e13d83d94b03d6020ce219d9a80de32b9dc
Jose <75870284+Jaro-c@users.noreply.github.com>
2025-04-17 19:52:32 -0700

    💄 style(toast): enhance success and error toast styles with transitions

--- src/components/UI/toaster.tsx
+++ src/components/UI/toaster.tsx
@@ -12,14 +12,14 @@ export default function LoadToaster() {
 					duration: 2000,
 					icon: <CircleCheck className="size-5 shrink-0 text-green-500" />,
 					className:
-						"z-50 flex items-center gap-2 bg-white text-gray-800 border border-green-300 shadow-md rounded-lg px-3 lg:px-4 py-2 lg:py-3 text-xs lg:text-sm xl:text-base font-medium select-none mx-2 lg:mx-4 mb-4",
+						"z-50 flex items-center gap-2 bg-white text-gray-800 border border-green-300 shadow-md transition-all duration-300 ease-in-out rounded-lg px-3 lg:px-4 py-2 lg:py-3 text-xs lg:text-sm xl:text-base font-medium select-none mx-2 lg:mx-4 mb-4",
 				},
 				loading: {},
 				error: {
 					duration: 4000,
 					icon: <CircleX className="size-5 shrink-0 text-red-500" />,
 					className:
-						"z-50 flex items-center gap-2 bg-white text-gray-800 border border-red-300 shadow-md rounded-lg px-3 lg:px-4 py-2 lg:py-3 text-xs lg:text-sm xl:text-base font-medium select-none mx-2 lg:mx-4 mb-4",
+						"z-50 flex items-center gap-2 bg-white text-gray-800 border border-red-300 shadow-md transition-all duration-300 ease-in-out rounded-lg px-3 lg:px-4 py-2 lg:py-3 text-xs lg:text-sm xl:text-base font-medium select-none mx-2 lg:mx-4 mb-4",
 				},
 			}}
 		/>
