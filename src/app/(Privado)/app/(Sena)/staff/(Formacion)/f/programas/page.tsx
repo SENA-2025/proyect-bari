b1f2d639d702fc219a09c19d43ce3ae772832d37
Jose <75870284+Jaro-c@users.noreply.github.com>
2025-04-18 13:03:49 -0700

    🔧 feat(staff): add metadata for Fichas, Modalidades, Niveles, Programas, and Transversales page components

--- src/app/(Privado)/app/(Sena)/staff/(Formacion)/f/programas/page.tsx
+++ src/app/(Privado)/app/(Sena)/staff/(Formacion)/f/programas/page.tsx
@@ -1,3 +1,11 @@
+import type { Metadata } from "next";
+
+// Metadata
+export const metadata: Metadata = {
+	title: "Programas de formación",
+};
+
+// Pagina
 export default function ProgramasPage() {
 	return null;
 }
--- src/app/(Privado)/app/(Sena)/staff/(Formacion)/f/transversales/page.tsx
+++ src/app/(Privado)/app/(Sena)/staff/(Formacion)/f/transversales/page.tsx
@@ -1,3 +1,11 @@
+import type { Metadata } from "next";
+
+// Metadata
+export const metadata: Metadata = {
+	title: "Transversales",
+};
+
+// Pagina
 export default function TransversalesPage() {
 	return null;
 }
