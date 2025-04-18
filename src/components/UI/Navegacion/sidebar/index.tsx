043ad21d8948be5add09e1493add9baa5c264ef9
Jose <75870284+Jaro-c@users.noreply.github.com>
2025-04-17 17:21:30 -0700

    🔧 refactor(sidebar): remove unnecessary <aside> wrapper from Sidebar component

--- src/components/UI/Navegacion/sidebar/index.tsx
+++ src/components/UI/Navegacion/sidebar/index.tsx
@@ -6,10 +6,8 @@ const Desktop = dynamic(() => import("@/components/UI/Navegacion/sidebar/desktop
 
 export default function Sidebar() {
 	return (
-		<aside className="hidden h-full lg:block">
-			<Suspense fallback={null}>
-				<Desktop />
-			</Suspense>
-		</aside>
+		<Suspense fallback={null}>
+			<Desktop />
+		</Suspense>
 	);
 }
