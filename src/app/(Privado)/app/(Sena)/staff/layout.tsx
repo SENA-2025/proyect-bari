67488d5f667e584c1e961d0501b411c0bff5fd69
Jose <75870284+Jaro-c@users.noreply.github.com>
2025-04-18 12:44:40 -0700

    🔧 feat(staff): add metadata for Staff layout component

--- src/app/(Privado)/app/(Sena)/staff/layout.tsx
+++ src/app/(Privado)/app/(Sena)/staff/layout.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from "next";
+
+// Metadata
+export const metadata: Metadata = {
+	title: "Staff",
+};
+
 // Layout
 export default function StaffLayout({ children }: Readonly<{ children: React.ReactNode }>) {
 	return children;
