a5274a3ab53ed1e4922154582b92d6186d9f4d7c
Jose <75870284+Jaro-c@users.noreply.github.com>
2025-04-17 17:19:32 -0700

    🔧 refactor(sidebar): simplify Suspense fallback to null

--- src/components/UI/Navegacion/sidebar/index.tsx
+++ src/components/UI/Navegacion/sidebar/index.tsx
@@ -7,17 +7,7 @@ const Desktop = dynamic(() => import("@/components/UI/Navegacion/sidebar/desktop
 export default function Sidebar() {
 	return (
 		<aside className="hidden h-full lg:block">
-			<Suspense
-				fallback={
-					<div className="z-10 flex h-full w-16 flex-col gap-2 bg-white px-2 py-4 shadow-lg">
-						{Array(3)
-							.fill(0)
-							.map((_, index) => (
-								<div className="h-12 w-full animate-pulse rounded-lg bg-gray-200" key={index}></div>
-							))}
-					</div>
-				}
-			>
+			<Suspense fallback={null}>
 				<Desktop />
 			</Suspense>
 		</aside>
