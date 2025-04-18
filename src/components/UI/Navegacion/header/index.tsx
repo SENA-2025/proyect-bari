906f4421a3dc04850734856a5cce2ed2582d4645
Jose <75870284+Jaro-c@users.noreply.github.com>
2025-04-18 14:11:15 -0700

    🔧 fix(header): adjust header height and improve link structure for better layout

--- src/components/UI/Navegacion/header/index.tsx
+++ src/components/UI/Navegacion/header/index.tsx
@@ -2,18 +2,17 @@ import Link from "next/link";
 
 export default function Header() {
 	return (
-		<header className="z-20 h-16 w-full bg-white shadow-sm">
+		<header className="z-20 h-14 w-full bg-white shadow-sm transition-all duration-300 ease-in-out lg:h-16">
 			<div className="flex size-full items-center">
 				<div className="mx-4 flex items-center justify-between">
 					{/* Mobile Nav + Título */}
-					<Link href="/app" className="select-none">
-						{/* Mobile */}
-
-						{/* PC */}
-						<span className="from-tertiary-600 to-primary-400 bg-gradient-to-r bg-clip-text text-lg font-bold text-transparent md:text-xl lg:text-2xl xl:text-3xl">
-							{process.env.APP_WEB_TITLE}
-						</span>
-					</Link>
+					<div className="flex items-center justify-center">
+						<Link href="/app">
+							<span className="from-tertiary-600 to-primary-400 bg-gradient-to-r bg-clip-text text-lg font-bold text-transparent transition-all duration-300 ease-in-out select-none md:text-xl lg:text-2xl xl:text-3xl">
+								{process.env.APP_WEB_TITLE}
+							</span>
+						</Link>
+					</div>
 
 					{/* Navegación */}
 					<div></div>
