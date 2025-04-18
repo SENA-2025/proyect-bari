a27ffd47cbff7e468370b732c2723cac6daf3087
Jose <75870284+Jaro-c@users.noreply.github.com>
2025-04-18 11:25:17 -0700

    🔧 feat(staff): implement StaffPage component with placeholder content

--- src/app/(Privado)/app/(Sena)/staff/page.tsx
+++ src/app/(Privado)/app/(Sena)/staff/page.tsx
@@ -1,3 +1,23 @@
 export default function StaffPage() {
-	return null;
+	return (
+		<div>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus libero est ea saepe. Impedit, ea velit eius quod rerum tempore
+			recusandae voluptates reprehenderit dolor facere. Laudantium officia minus excepturi voluptatum? Lorem ipsum dolor sit amet consectetur
+			adipisicing elit. Accusamus libero est ea saepe. Impedit, ea velit eius quod rerum tempore recusandae voluptates reprehenderit dolor
+			facere. Laudantium officia minus excepturi voluptatum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus libero est ea
+			saepe. Impedit, ea velit eius quod rerum tempore recusandae voluptates reprehenderit dolor facere. Laudantium officia minus excepturi
+			voluptatum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus libero est ea saepe. Impedit, ea velit eius quod rerum
+			tempore recusandae voluptates reprehenderit dolor facere. Laudantium officia minus excepturi voluptatum? Lorem ipsum dolor sit amet
+			consectetur adipisicing elit. Accusamus libero est ea saepe. Impedit, ea velit eius quod rerum tempore recusandae voluptates reprehenderit
+			dolor facere. Laudantium officia minus excepturi voluptatum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus libero est
+			ea saepe. Impedit, ea velit eius quod rerum tempore recusandae voluptates reprehenderit dolor facere. Laudantium officia minus excepturi
+			voluptatum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus libero est ea saepe. Impedit, ea velit eius quod rerum
+			tempore recusandae voluptates reprehenderit dolor facere. Laudantium officia minus excepturi voluptatum? Lorem ipsum dolor sit amet
+			consectetur adipisicing elit. Accusamus libero est ea saepe. Impedit, ea velit eius quod rerum tempore recusandae voluptates reprehenderit
+			dolor facere. Laudantium officia minus excepturi voluptatum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus libero est
+			ea saepe. Impedit, ea velit eius quod rerum tempore recusandae voluptates reprehenderit dolor facere. Laudantium officia minus excepturi
+			voluptatum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus libero est ea saepe. Impedit, ea velit eius quod rerum
+			tempore recusandae voluptates reprehenderit dolor facere. Laudantium officia minus excepturi voluptatum?
+		</div>
+	);
 }
