1c34babafff80690e8e29bdb6c7fdab9b49f7c17
Jose <75870284+Jaro-c@users.noreply.github.com>
2025-04-27 12:11:35 -0700

    🎨 style(header): reorganize header layout for improved structure and alignment

--- src/app/(Privado)/app/(Sena)/staff/(Coordinacion)/c/regionales/page.tsx
+++ src/app/(Privado)/app/(Sena)/staff/(Coordinacion)/c/regionales/page.tsx
@@ -16,25 +16,27 @@ export default function RegionalesPage() {
 	return (
 		<div className="size-full p-5">
 			{/* Cabecera */}
-			<div className="flex w-full flex-col items-start justify-between gap-4 rounded-xl border border-gray-100 bg-white p-4 shadow-sm transition-all duration-300 ease-in-out lg:flex-row lg:items-center lg:p-6">
-				{/* Título */}
-				<div className="flex w-full flex-col items-center gap-2 transition-all duration-300 ease-in-out lg:flex-row lg:justify-start">
-					{/* Icono */}
-					<div className="from-primary-400 to-tertiary-600 rounded-lg bg-gradient-to-r p-2">
-						<Globe className="size-5 text-white transition-all duration-300 ease-in-out lg:size-6" />
-					</div>
-
+			<div className="w-full rounded-xl border border-gray-100 bg-white p-4 shadow-sm transition-all duration-300 ease-in-out lg:p-6">
+				<div className="flex flex-col items-center justify-between gap-4 transition-all duration-300 ease-in-out lg:flex-row">
 					{/* Título */}
-					<div className="flex flex-col items-center justify-center transition-all duration-300 ease-in-out select-none lg:items-start">
-						<h1 className="text-xl font-bold text-gray-900 transition-all duration-300 ease-in-out lg:text-2xl">Regionales</h1>
-						<p className="text-sm text-gray-500">Gestiona las regionales del SENA</p>
-					</div>
-				</div>
+					<div className="flex flex-col items-center justify-center gap-2 transition-all duration-300 ease-in-out lg:flex-row">
+						{/* Icono */}
+						<div className="from-primary-400 to-tertiary-600 rounded-lg bg-gradient-to-r p-2">
+							<Globe className="size-5 text-white transition-all duration-300 ease-in-out lg:size-6" />
+						</div>
 
-				{/* Botones */}
-				<Suspense fallback={null}>
-					<Buttons />
-				</Suspense>
+						{/* Título */}
+						<div className="flex flex-col items-center justify-center transition-all duration-300 ease-in-out select-none lg:items-start">
+							<h1 className="text-xl font-bold text-gray-900 transition-all duration-300 ease-in-out lg:text-2xl">Regionales</h1>
+							<p className="text-sm text-gray-500">Gestiona las regionales del SENA</p>
+						</div>
+					</div>
+
+					{/* Botones */}
+					<Suspense fallback={null}>
+						<Buttons />
+					</Suspense>
+				</div>
 			</div>
 
 			{/* Contenido */}
