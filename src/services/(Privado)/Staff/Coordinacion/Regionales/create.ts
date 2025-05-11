27aab85b0ad4f8a37c0ad67074871b65c0415883
Jose <75870284+Jaro-c@users.noreply.github.com>
2025-05-10 21:03:30 -0700

    ✨ feat(service): implement ServiceCreate function with validation and error handling

--- src/services/(Privado)/Staff/Coordinacion/Regionales/create.ts
+++ src/services/(Privado)/Staff/Coordinacion/Regionales/create.ts
@@ -1,7 +1,46 @@
 "use server";
 
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { ZodError } from "zod";
+
+import createSchema from "@/schemas/(Privado)/Staff/Coordinacion/Regionales/create.schema";
+
 // Tipos
-export type ServiceType = {};
+export type ServiceType = {
+	eventId?: number;
+	error: boolean;
+	message?: string;
+};
 
 // Validación de Datos
-export async function ServiceCreate(formData: FormData): Promise<ServiceType> {}
+export async function ServiceCreate(formData: FormData): Promise<ServiceType> {
+	const cookieStore = await cookies();
+
+	// Validar si existe el Refresh Token
+	if (!cookieStore.has("__srfk")) {
+		redirect("/acceder");
+	}
+
+	// Validar si existe el Access Token
+	if (!cookieStore.has("_sid")) {
+	}
+
+	try {
+		// Validar Datos
+		const data = createSchema.parse(Object.fromEntries(formData.entries()));
+
+		console.log(data);
+
+		// Enviar Datos
+		return { error: false };
+	} catch (error) {
+		// Error de Validación
+		if (error instanceof ZodError) {
+			return { eventId: Date.now(), error: true, message: error.issues.shift()?.message || "Datos inválidos." };
+		}
+
+		// Error Interno
+		return { eventId: Date.now(), error: true, message: "Error interno. Inténtalo más tarde." };
+	}
+}
