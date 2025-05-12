274e3f4e043790a1778eab6edccd148628af9d4f
Jose Vera <208693353+ToseDera@users.noreply.github.com>
2025-05-12 16:34:42 -0500

    Creation of a page for all registration sections and integration of description and logo

---
+++ src/app/(Privado)/app/(Sena)/staff/(Registros)/r/formacion/page.tsx
@@ -0,0 +1,40 @@
+import { FileText } from "lucide-react";
+import type { Metadata } from "next";
+
+// Metadata
+export const metadata: Metadata = {
+	title: "Formación",
+};
+
+// Pagina
+export default function FormacionPage() {
+	return (
+		<div className="size-full p-5">
+			{/* Cabecera */}
+			<div className="w-full rounded-xl border border-gray-100 bg-white p-4 shadow-sm transition-all duration-300 ease-in-out lg:p-6">
+				<div className="flex flex-col items-center justify-between gap-4 transition-all duration-300 ease-in-out lg:flex-row">
+					{/* Título */}
+					<div className="flex flex-col items-center justify-center gap-2 transition-all duration-300 ease-in-out lg:flex-row">
+						{/* Icono */}
+						<div className="from-primary-400 to-tertiary-600 rounded-lg bg-gradient-to-r p-2">
+							<FileText className="size-5 text-white transition-all duration-300 ease-in-out lg:size-6" />
+						</div>
+
+						{/* Título */}
+						<div className="flex flex-col items-center justify-center transition-all duration-300 ease-in-out select-none lg:items-start">
+							<h1 className="text-xl font-bold text-gray-900 transition-all duration-300 ease-in-out lg:text-2xl">Formación</h1>
+							<p className="text-sm text-gray-500">Registra y gestiona datos de formación del SENA</p>
+						</div>
+					</div>
+
+					{/* Botones */}
+					{/* <Suspense fallback={null}>
+				<Buttons />
+			</Suspense> */}
+				</div>
+			</div>
+
+			{/* Contenido */}
+		</div>
+	);
+}
