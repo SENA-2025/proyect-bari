fcaf07d8be171935a6e0c2834cc21992854babf2
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2025-05-15 15:13:08 -0500

    Refactor InstructoresPage component to enable dynamic loading of Buttons component

--- src/app/(Privado)/app/(Sena)/staff/(Coordinacion)/c/instructores/page.tsx
+++ src/app/(Privado)/app/(Sena)/staff/(Coordinacion)/c/instructores/page.tsx
@@ -1,5 +1,10 @@
 import { GraduationCap } from "lucide-react";
 import type { Metadata } from "next";
+import dynamic from "next/dynamic";
+import { Suspense } from "react";
+
+// Componentes
+const Buttons = dynamic(() => import("@/components/(Privado)/Staff/Coordinacion/Instructores/buttons"));
 
 // Metadata
 export const metadata: Metadata = {
@@ -28,9 +33,9 @@ export default function InstructoresPage() {
 					</div>
 
 					{/* Botones */}
-					{/* <Suspense fallback={null}>
+					<Suspense fallback={null}>
 						<Buttons />
-					</Suspense> */}
+					</Suspense>
 				</div>
 			</div>
 
