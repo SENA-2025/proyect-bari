8363c01fe4e2d58b4e2c577da0a5a185aaf4aee5
Jose <75870284+Jaro-c@users.noreply.github.com>
2025-05-15 20:13:54 -0700

    update title casing in ProgramasPage for consistency

--- src/app/(Privado)/app/(Sena)/staff/(Formacion)/f/programas/page.tsx
+++ src/app/(Privado)/app/(Sena)/staff/(Formacion)/f/programas/page.tsx
@@ -28,7 +28,7 @@ export default function ProgramasPage() {
 						{/* Título */}
 						<div className="flex flex-col items-center justify-center transition-all duration-300 ease-in-out select-none lg:items-start">
 							<h1 className="text-xl font-bold text-gray-900 transition-all duration-300 ease-in-out lg:text-2xl">
-								Programas de formación
+								Programas de Formación
 							</h1>
 							<p className="text-sm text-gray-500">Gestiona los programas de formación del SENA</p>
 						</div>
