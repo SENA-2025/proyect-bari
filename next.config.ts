20b24e7373e02d1c6ce10f59e581015cfd158fae
Jose <75870284+Jaro-c@users.noreply.github.com>
2026-03-06 11:28:56 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,6 @@ const nextConfig: NextConfig = {
 	devIndicators: false, // No mostrar indicadores de desarrollo
 	poweredByHeader: false, // No mostrar el encabezado de Next.js
 	generateEtags: false, // No generar ETags (Cache gestionada por Cloudflare)
-	experimental: {
-		optimizePackageImports: ["uuid", "zod", "react-hot-toast", "undici", "use-debounce", "clsx", "tailwind-merge"],
-	},
 	images: {
 		remotePatterns: [],
 	},
